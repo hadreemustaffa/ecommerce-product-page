c72a0d1e6506110f482d5b39d7f5e86121282484
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-01-03 16:16:18 +0800

    add Product component files

---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,18 @@
+function ProductImage() {
+  return <div></div>;
+}
+
+function ProductDetails() {
+  return <div></div>;
+}
+
+function Product() {
+  return (
+    <>
+      <ProductImage />
+      <ProductDetails />
+    </>
+  );
+}
+
+export default Product;
