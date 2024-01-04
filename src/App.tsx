0a8a8096f87a13c09d0f0f18565cd4282fc5a1aa
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-01-04 18:28:59 +0800

    add components and basic states

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Product from "./components/Product/Product";
 
-import './App.css'
+export default function App() {
+  const [itemCount, setItemCount] = useState(0);
+  const [isEmpty, setIsEmpty] = useState(false);
 
-function App() {
+  function handleCount(e: React.MouseEvent<HTMLButtonElement>) {
+    if (e.target === document.querySelector("#btnAddItem")) {
+      setItemCount(itemCount + 1);
+    }
+    if (e.target === document.querySelector("#btnRemoveItem")) {
+      setItemCount(itemCount - 1);
+    }
+    if (itemCount > -1) {
+      setIsEmpty(true);
+    }
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+    <>
+      <Header isEmpty={isEmpty} itemCount={itemCount} />
+      <Product />
+    </>
+  );
 }
-
-export default App
