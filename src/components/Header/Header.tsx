29006271bc24bf0a3f6ac47a76e4bbb926cb6e82
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-01-17 18:32:45 +0800

    style: add rules for desktop design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,16 @@ interface Content {
   children: React.ReactNode;
 }
 
+const BrandLogo = () => {
+  return (
+    <>
+      <a href="/">
+        <img src={brandLogo} alt="" />
+      </a>
+    </>
+  );
+};
+
 const Header = ({ count, children }: Content) => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -19,12 +29,10 @@ const Header = ({ count, children }: Content) => {
   }
 
   return (
-    <header className="flex flex-row justify-between gap-12 p-6">
-      <div className="flex flex-row items-center gap-6">
+    <header className="flex flex-row justify-between gap-12 p-6 xl:border-b-grayish-blue xl:px-0 xl:pb-8 xl:pt-7">
+      <div className="flex flex-row items-center gap-6 xl:flex-row-reverse xl:gap-14 ">
         <NavigationBar />
-        <a href="/">
-          <img src={brandLogo} alt="" />
-        </a>
+        <BrandLogo />
       </div>
 
       <div className="flex flex-row gap-6">
--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import menuIconOpen from "/images/icon-menu.svg";
 import menuIconClose from "/images/icon-close.svg";
+import { navLinks } from "../../constants";
 
 const NavigationBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,13 +11,11 @@ const NavigationBar = () => {
     setIsOpen(!isOpen);
   }
 
-  const NavLinks = ["Collections", "Men", "Women", "About", "Contact"];
-
   return (
     <>
       <button
         onClick={handleToggle}
-        className="relative z-20 w-fit cursor-pointer"
+        className="relative z-20 w-fit cursor-pointer xl:hidden"
         aria-controls="navBar"
         aria-expanded="false"
         type="button"
@@ -33,18 +32,21 @@ const NavigationBar = () => {
         id="navBar"
         className={`absolute left-0 top-0 z-10 w-full  transition-transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
+        } xl:static xl:translate-x-0 `}
       >
-        <ul className="flex h-screen w-4/6 flex-col gap-4 bg-white px-6 pt-20">
-          {NavLinks.map((link) => (
+        <ul className="flex h-screen w-4/6 flex-col gap-4 bg-white px-6 pt-20 xl:h-fit xl:w-full xl:flex-row xl:justify-between xl:px-0 xl:pt-0">
+          {navLinks.map((link) => (
             <li key={link}>
-              <a href="#" className="flex font-bold">
+              <a
+                href="#"
+                className="flex font-bold xl:font-normal xl:text-grayish-blue-dark"
+              >
                 {link}
               </a>
             </li>
           ))}
         </ul>
-        <div className="absolute left-0 top-0 -z-10 h-full  w-full bg-black opacity-75"></div>
+        <div className="absolute left-0 top-0 -z-10 h-full w-full  bg-black opacity-75 xl:hidden"></div>
       </div>
     </>
   );
