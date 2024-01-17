c1521ceec683d933c6a2e1c061b8abbc53db99f0
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-01-17 18:24:01 +0800

    move checkout button to CartItem component

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,26 +1,12 @@
 import { PropsWithChildren } from "react";
 
-interface CartProps {
-  checkout: () => void;
-}
-
-const Cart = ({ checkout, children }: PropsWithChildren<CartProps>) => {
+const Cart = ({ children }: PropsWithChildren) => {
   return (
     <div className="absolute left-0 top-[4.5rem] z-[1] w-full">
-      <div className="m-2 overflow-hidden rounded-lg  bg-white shadow-md">
-        <p className="p-6 font-bold">Cart</p>
+      <div className="m-2 flex flex-col gap-6 overflow-hidden rounded-lg bg-white px-6 py-8 shadow-md">
+        <p className="font-bold">Cart</p>
         <div className="w-full border-b-2 border-y-grayish-blue-dark opacity-10"></div>
-        <div className="flex flex-col gap-6 p-6">
-          {children}
-
-          <button
-            onClick={checkout}
-            className="flex w-full justify-center rounded-lg bg-orange p-4 font-bold text-white"
-            type="submit"
-          >
-            Checkout
-          </button>
-        </div>
+        {children}
       </div>
     </div>
   );
