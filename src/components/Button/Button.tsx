43008fa4bbd8e4f48b457531b0a90227ee538467
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-01-24 14:27:47 +0800

    add button label as props and remove redundant button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,6 +3,7 @@ import { MouseEventHandler } from "react";
 interface ButtonProps {
   side?: string;
   disabled?: boolean;
+  label?: string;
   iconPath: string;
   onClick: () => void | MouseEventHandler<HTMLButtonElement>;
 }
@@ -10,13 +11,15 @@ interface ButtonProps {
 export const ProductImageButton = ({
   side,
   iconPath,
+  label,
   onClick,
 }: ButtonProps) => {
   return (
     <button
       type="button"
+      aria-label={label}
       onClick={onClick}
-      className={`absolute md:hidden ${side} top-0 flex h-full items-center p-4`}
+      className={`absolute ${side} top-0 flex h-full items-center p-4`}
     >
       <div className="flex h-[42px] w-[42px] items-center justify-center rounded-full bg-white">
         <img src={iconPath} width={10} height={14} alt="" />
@@ -28,36 +31,18 @@ export const ProductImageButton = ({
 export const ItemCountButton = ({
   iconPath,
   onClick,
+  label,
   disabled,
 }: ButtonProps) => {
   return (
     <button
+      aria-label={label}
       onClick={onClick}
       type="button"
       disabled={disabled}
       className="disabled:opacity-50"
     >
       <img src={iconPath} alt="" />
-    </button>
-  );
-};
-
-export const AddToCartButton = ({ onClick }: ButtonProps) => {
-  return (
-    <button
-      onClick={onClick}
-      type="button"
-      className="flex w-full flex-row items-center justify-center gap-2 rounded-lg bg-orange p-4 font-bold text-white"
-    >
-      <svg
-        width="22"
-        height="20"
-        className="fill-white"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" />
-      </svg>
-      Add To Cart
     </button>
   );
 };
