70b679772416dabc539bd2717884146b99f2005d
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-01-24 14:34:43 +0800

    style: minor style change and add render condition

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import menuIconOpen from "/images/icon-menu.svg";
 import menuIconClose from "/images/icon-close.svg";
 import { navLinks } from "../../constants";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 
 const NavigationBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   function handleToggle() {
     setIsOpen(!isOpen);
@@ -13,28 +15,31 @@ const NavigationBar = () => {
 
   return (
     <>
-      <button
-        onClick={handleToggle}
-        className="relative z-20 w-fit cursor-pointer lg:hidden"
-        aria-controls="navBar"
-        aria-expanded="false"
-        type="button"
-      >
-        <img
-          src={isOpen ? menuIconClose : menuIconOpen}
-          width={16}
-          height={16}
-          alt=""
-        />
-      </button>
+      {isMobile && (
+        <button
+          onClick={handleToggle}
+          className="relative z-20 w-fit cursor-pointer lg:hidden"
+          aria-label="Navigation Menu"
+          aria-controls="navBar"
+          aria-expanded="false"
+          type="button"
+        >
+          <img
+            src={isOpen ? menuIconClose : menuIconOpen}
+            width={16}
+            height={16}
+            alt=""
+          />
+        </button>
+      )}
 
       <div
         id="navBar"
-        className={`absolute left-0 top-0 z-10 w-full  transition-transform lg:static lg:h-full lg:translate-x-0 ${
+        className={`absolute left-0 top-0 z-10 w-full transition-transform md:static md:h-full md:translate-x-0 ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        <ul className="flex h-screen w-4/6 flex-col gap-4 bg-white px-6 pt-20 lg:h-full lg:w-full lg:flex-row lg:justify-between lg:px-0 lg:pt-0">
+        <ul className="flex h-screen w-4/6 flex-col gap-4 bg-white px-6 pt-20 md:h-full md:w-full md:flex-row md:justify-between md:gap-2 md:px-0 md:pt-0">
           {navLinks.map((link) => (
             <li
               key={link}
@@ -42,7 +47,7 @@ const NavigationBar = () => {
             >
               <a
                 href="#"
-                className="flex font-bold lg:font-normal lg:text-grayish-blue-dark lg:hover:after:absolute lg:hover:after:bottom-0 lg:hover:after:h-1 lg:hover:after:w-full lg:hover:after:bg-orange"
+                className="flex font-bold md:font-normal md:text-grayish-blue-dark md:hover:text-black lg:hover:after:absolute lg:hover:after:bottom-0 lg:hover:after:h-1 lg:hover:after:w-full lg:hover:after:bg-orange"
               >
                 {link}
               </a>
