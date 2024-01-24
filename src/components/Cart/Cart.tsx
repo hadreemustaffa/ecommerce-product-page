60884d73b21185749f68f7a1e8f7d124721f0a3e
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-01-24 14:28:45 +0800

    style: add style for screen larger than mobile

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 const Cart = ({ children }: PropsWithChildren) => {
   return (
-    <div className="absolute left-0 top-[4.5rem] z-[1] w-full">
+    <div className="absolute left-0 top-20 z-[1] w-full md:left-auto md:right-20 md:max-w-96">
       <div className="m-2 flex flex-col gap-6 overflow-hidden rounded-lg bg-white px-6 py-8 shadow-md">
         <p className="font-bold">Cart</p>
         <div className="w-full border-b-2 border-y-grayish-blue-dark opacity-10"></div>
